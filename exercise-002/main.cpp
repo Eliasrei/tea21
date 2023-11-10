384e86b602c4d8c2ee751eeb08b8fe1d73ac7d42
Eliasrei <149793800+Eliasrei@users.noreply.github.com>
2023-11-10 12:50:41 +0100

    sort the vector

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -2,42 +2,56 @@
 #include <fmt/format.h>
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <random>  // for std::random_device, std::mt19937, std::uniform_int_distribution
 #include <algorithm>
+
 #include "CLI/CLI.hpp"
 #include "config.h"
 
 auto main(int argc, char **argv) -> int
 {
-    /**
-     * CLI11 is a command line parser to add command line options
-     * More info at https://github.com/CLIUtils/CLI11#usage
-     */
     CLI::App app{PROJECT_NAME};
     int count = 20;
     app.add_option("-c,--count", count, "Set the count parameter")->check(CLI::PositiveNumber);
+
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-       
+        app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
-    
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
+
     fmt::print("Hello, {}! Count: {}\n", app.get_name(), count);
 
-    /* INSERT YOUR CODE HERE */
-     std::srand(std::time(nullptr)); // Seed für die Zufallsgenerierung
+    // Use <random> for better randomness
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distribution(1, 100);
+
     std::vector<int> randomValues(count);
 
+    for (int &value : randomValues) {
+        value = distribution(gen);
+    }
 
-    return 0; /* exit gracefully*/
+    fmt::print("Random values: ");
+    for (const int &value : randomValues) {
+        fmt::print("{} ", value);
+    }
+    fmt::print("\n");
+
+    // Sort the vector of random values
+    std::sort(randomValues.begin(), randomValues.end());
+
+    // Print the sorted values
+    fmt::print("Sorted values: ");
+    for (const int &value : randomValues) {
+        fmt::print("{} ", value);
+    }
+    fmt::print("\n");
+
+    return 0; /* exit gracefully */
 }
