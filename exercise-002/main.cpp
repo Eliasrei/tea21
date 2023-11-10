ac09bb3bd6e6b3e37f52dad273aaebfc0d744cfc
Eliasrei <149793800+Eliasrei@users.noreply.github.com>
2023-11-10 10:54:52 +0100

    Add -c,--count option and generate random values

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,5 +1,9 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 #include "CLI/CLI.hpp"
 #include "config.h"
@@ -12,17 +16,17 @@ auto main(int argc, char **argv) -> int
      */
     CLI::App app{PROJECT_NAME};
     int count = 20;
-    app.add_option("-c,--count", count, "Set the count parameter")->check(CLI::PositiveNumber);
+    
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app.parse(argc, argv);
+        app.add_option("-c,--count", count, "Set the count parameter");
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
-
+    
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
@@ -31,6 +35,19 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}! Count: {}\n", app.get_name(), count);
 
     /* INSERT YOUR CODE HERE */
+     std::srand(std::time(0)); // Seed für die Zufallsgenerierung
+    std::vector<int> randomValues(count);
+
+    for (int &value : randomValues) {
+        value = std::rand() % 100 + 1; // Zufallszahl von 1 bis 100 generieren
+    }
+
+    // Ausgabe der zufälligen Werte
+    fmt::print("Random values: ");
+    for (const int &value : randomValues) {
+        fmt::print("{} ", value);
+    }
+    fmt::print("\n");
 
     return 0; /* exit gracefully*/
 }
